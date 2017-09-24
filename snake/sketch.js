72aa284472946cc83e83a62579171fe2dd466bf3
oaza <oaza@MacBook-Air-oaza.local>
2017-09-24 21:09:48 +0200

    snake

--- snake/sketch.js
+++ snake/sketch.js
@@ -1,21 +1,41 @@
-var circleX = 0;
-var circleY = 0;
+var s;
 function setup() {
 
 	createCanvas (600, 600);
-    background(125,250,55); 
+    s = new Snake();
+
 }
 function draw(){
-    fill(255,111,250,100);
-    noStroke();
-	ellipse(circleX, circleY, 20, 20);
-    circleX = circleX + 1;
-    circleY = circleY + 1;
+    background(51);
+    s.update();
+    s.show();
 }
 
-function mousePressed(){
-    background(125,250,55); 
-    circleX = mouseX;
-    circleY = mouseY;
+function keyPressed() {
     
+    if(keyCode === UP_ARROW){
+        s.dir(0, -1);
+    }else if(keyCode === DOWN_ARROW){
+        s.dir(0,1);
+    }else if(keyCode === RIGHT_ARROW){
+        s.dir(1,0);
+    else if(keyCode === LEFT_ARROW){
+        s.dir(-1,0);
+}
+
+function Snake(){
+    this.x = 0;
+    this.y = 0;
+    this.xspeed = 1;
+    this.yspeed = 0;
+}
+
+this update = function(){
+    this.x = this.x + this.xspeed;
+    this.y = this.y + this.yspeed;
+}
+
+this.show = function(){
+    fill(255);
+    rect(this.x, this.y,10,10);
 }
