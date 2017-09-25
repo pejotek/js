5b1076cc2913ff8a9f3ff2d003b990617f61960d
oaza <oaza@MacBook-Air-oaza.local>
2017-09-25 20:44:41 +0200

    25-09

--- snake/sketch.js
+++ snake/sketch.js
@@ -1,14 +1,33 @@
 var s;
+var scl = 20;
+var food;
+
 function setup() {
 
 	createCanvas (600, 600);
     s = new Snake();
+    frameRate(10);
+    pickLocation();
+}
 
+function pickLocation(){
+    var cols = floor(width/scl);
+    var rows = floor(height/scl);
+    food = createVector(floor(random(cols)), floor(random(rows)));
+    food.mult(scl);
 }
 function draw(){
     background(51);
     s.update();
     s.show();
+    
+    if(s.eat(food)){
+        pickLocation();   
+    }
+    
+    fill(255,0,100);
+    //noStroke();
+    rect(food.x, food.y,scl,scl);
 }
 
 function keyPressed() {
@@ -19,23 +38,9 @@ function keyPressed() {
         s.dir(0,1);
     }else if(keyCode === RIGHT_ARROW){
         s.dir(1,0);
-    else if(keyCode === LEFT_ARROW){
+    }else if(keyCode === LEFT_ARROW){
         s.dir(-1,0);
 }
-
-function Snake(){
-    this.x = 0;
-    this.y = 0;
-    this.xspeed = 1;
-    this.yspeed = 0;
 }
 
-this update = function(){
-    this.x = this.x + this.xspeed;
-    this.y = this.y + this.yspeed;
-}
 
-this.show = function(){
-    fill(255);
-    rect(this.x, this.y,10,10);
-}
